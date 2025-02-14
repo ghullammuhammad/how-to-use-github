1253593992c83a73c338708d6365113ad70dff9c
ghullammuhammad <731326@student.inholland.nl>
2025-02-14 12:43:48 +0100

    hhjkj
    nghkhhk

---
+++ how to use github/how to use github/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace how_to_use_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
